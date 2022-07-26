e6e54331bfd8d564d81f0b6880cdf261b9bcb129
Пащенко Владимир <pikimelposhta@gmail.com>
2022-07-26 23:03:18 +0300

    Clear Branch

--- module3/lesson_2/dz/index10.js
+++
@@ -1,9 +0,0 @@
-function add(...args) {
-  let sum = 0;
-
-  for (let num of args) {
-    sum += num;
-  }
-
-  return sum;
-}
--- module3/lesson_2/dz/index5.js
+++
@@ -1,8 +0,0 @@
-function calculateMeanTemperature(forecast) {
-  let {
-    today: { low: todayLow, high: todayHigh },
-    tomorrow: { low: tomorrowLow, high: tomorrowHigh },
-  } = forecast;
-
-  return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
-}
