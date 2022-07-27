208191a48e516c58148198b12998c69c5c7fd96c
Пащенко Владимир <pikimelposhta@gmail.com>
2022-07-27 09:59:19 +0300

    less1

---
+++ lesson1/script1.js
@@ -0,0 +1,15 @@
+/*
+## Array
+
+- [#Array #8kyu task1](https://www.codewars.com/kata/515e271a311df0350d00000f)
+- [#Array #8kyu task2](https://www.codewars.com/kata/576bb71bbbcf0951d5000044)
+- [#Array #8kyu task3](https://www.codewars.com/kata/57d814e4950d8489720008db)
+- [#Array #8kyu task4](https://www.codewars.com/kata/576b93db1129fcf2200001e6)
+
+## String
+
+- [#String #8kyu task1](https://www.codewars.com/kata/57eae20f5500ad98e50002c5)
+- [#String #8kyu task2](https://www.codewars.com/kata/53369039d7ab3ac506000467)
+- [#String #8kyu task3](https://www.codewars.com/kata/5265326f5fda8eb1160004c8)
+- [#String #8kyu task4](https://www.codewars.com/kata/5808e2006b65bff35500008f)
+*/
