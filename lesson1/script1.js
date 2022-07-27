e6bdd7f2f592479dd429a329066b3c513c845a6d
Пащенко Владимир <pikimelposhta@gmail.com>
2022-07-27 12:57:24 +0300

    lesson-1 example

--- lesson1/script1.js
+++ lesson1/script1.js
@@ -1,15 +1,19 @@
-/*
-## Array
+let array = [2, 57, 23, 8, 6, 3, 7];
 
-- [#Array #8kyu task1](https://www.codewars.com/kata/515e271a311df0350d00000f)
-- [#Array #8kyu task2](https://www.codewars.com/kata/576bb71bbbcf0951d5000044)
-- [#Array #8kyu task3](https://www.codewars.com/kata/57d814e4950d8489720008db)
-- [#Array #8kyu task4](https://www.codewars.com/kata/576b93db1129fcf2200001e6)
+function foo(array) {
+  let i = 0;
+  let array1 = [...array];
+  for (let value of array1) {
+    if (value % 2 !== 0) {
+      let index = array1.indexOf(value) - i++;
+      array.splice(index, 1);
+    }
+  }
+}
 
-## String
+// function foo(array) {
+//     return array.filter((value) => value % 2 === 0);
+// }
 
-- [#String #8kyu task1](https://www.codewars.com/kata/57eae20f5500ad98e50002c5)
-- [#String #8kyu task2](https://www.codewars.com/kata/53369039d7ab3ac506000467)
-- [#String #8kyu task3](https://www.codewars.com/kata/5265326f5fda8eb1160004c8)
-- [#String #8kyu task4](https://www.codewars.com/kata/5808e2006b65bff35500008f)
-*/
+foo(array);
+console.log(array);
