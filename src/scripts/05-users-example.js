7db9fec9fd28f17548e05c3533223bcb7b0fee29
Пащенко Владимир <pikimelposhta@gmail.com>
2022-08-09 17:42:19 +0300

    last update

--- src/scripts/05-users-example.js
+++ src/scripts/05-users-example.js
@@ -12,20 +12,26 @@ setTimeout(() => {
   loadData();
   MainFunction();
 }, 1000);
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Object references
 let refs = {
   userList: document.querySelector(".js-list-user"),
   userCard: document.querySelector(".js-user-card"),
   backDrop: document.querySelector(".js-backdrop"),
   userInfo: document.querySelector(".js-user-info"),
+  firstModal: document.querySelector(".js-user-info"),
+  secondModal: document.querySelector(".js-form-regestration"),
 };
+
+// Функція завантажує дані про користувачів до списку
 function loadData() {
-  let result = users.map((user, index) => {
+  let result = users.map((user) => {
     return ` 
 <li class="user-card js-user-card" data-id=${user.id}>
   <div class="snip1344">
-      <img src="https://source.unsplash.com/100x100/?random=${index}&avatar,person,boy,girl" alt="profile-sample1" class="background"/>
-      <img src="https://source.unsplash.com/100x100/?random=${index}&avatar,person,boy,girl" alt="profile-sample1" class="profile"/>
+      <img src="https://source.unsplash.com/100x100/?random=${user.id}&avatar,person,boy,girl" alt="profile-sample1" class="background"/>
+      <img src="https://source.unsplash.com/100x100/?random=${user.id}&avatar,person,boy,girl" alt="profile-sample1" class="profile"/>
       <figcaption>
         <h3>${user.name}<span>${user.phone}</span></h3>
           <div>@${user.username}</div>
@@ -34,33 +40,66 @@ function loadData() {
 </li>
 `;
   });
-
   refs.userList.innerHTML = result.join("");
 }
+
+//Головна функція яка все об'єднала
 function MainFunction() {
   ////////////////////////////////////////////////////////////////////
+
+  // Створюємо додаткові посилання на необхідні елементи
   refs.userCard = document.querySelector(".js-list-user");
 
-  refs.userCard.addEventListener("click", onUserCardClick);
-  refs.backDrop.addEventListener("click", onbackDropToClose);
+  // Додаємо слухачі подій на елементи
+  refs.userCard.addEventListener("click", onUserCardClick); // Відкриття модалки при кліку на Юсера
+  refs.backDrop.addEventListener("click", onbackDropToClose); // Закриття модалки при  кліку на бекдроп
 
+  // Інструкція відкриття модалки
   function onUserCardClick(e) {
     document.body.classList.add("show-modal");
 
-    let myTarget = e.target;
-    while (myTarget.nodeName !== "LI") {
-      myTarget = myTarget.parentNode;
-    }
-    let id = myTarget.dataset.id;
-    let user = users.find((elem) => elem.id == id);
+    if (users.length == 0) {
+      // якщо коритсувачів немає в массиві (порожній список)
+      refs.userInfo.classList.remove("visible");
+      refs.secondModal.classList.add("visible");
+    } else {
+      //якщо користувачі є (список НЕ порожній)
 
-    loadUserData(user);
-    refs.inputDel = document.querySelector(".js-delete-user-btn");
-    refs.inputDel.addEventListener("click", onCardDelete);
-    function onCardDelete() {
-      console.log(id);
+      refs.userInfo.classList.add("visible");
+      refs.secondModal.classList.remove("visible");
+
+      // Отримую посилання на головний елемент юзера (тег - ЛІ)
+      let myTarget = e.target;
+      while (myTarget.nodeName !== "LI") {
+        myTarget = myTarget.parentNode;
+      }
+      let id = myTarget.dataset.id; // отримую АйДі
+
+      let user = users.find((elem) => elem.id == id); // Отримую юзера з массиву юзерів (лише того у якого задовільняє айді)
+
+      loadUserData(user); //Викликаю функцію яка завантаже до модального вікна інформацію про Юзера
+
+      // отримую посилання на кнопку для видалення юзера у модальному вікні
+      refs.inputDel = document.querySelector(".js-delete-user-btn");
+
+      //Додаю до цієї кнопки прослуховувач події Клік
+      refs.inputDel.addEventListener("click", onCardDelete);
+
+      // Описую коллбек для прослуховувача події у "inputDel"
+      function onCardDelete() {
+        // Отримую відфільтрованний массив користувачів які задовільняють умову
+        users = users.filter((user) => user.id != id);
+
+        // Після видалення юзеру приховую модальне вікно
+        document.body.classList.remove("show-modal");
+
+        //Викликаю функцію рендера юзерів у списку за оновленним массивом юзерів
+        loadData();
+      }
     }
   }
+
+  // Функція яка завантажує данні про Юзера до модального вікна
   function loadUserData(user) {
     refs.userInfo.innerHTML = `
     <div class="box box-gorizont">
@@ -81,32 +120,11 @@ ${Object.keys(user.address)
   .join("")}
 </div>`;
   }
+
+  // Колбек функція для закриття модального вікна
   function onbackDropToClose(eve) {
     if (eve.currentTarget === eve.target)
       document.body.classList.remove("show-modal");
   }
   ///////////////////////////////////////////////////////////////////
 }
-
-/* 
-<div class="tomato box box-gorizont">
-    <img src="https://source.unsplash.com/100x100/?random=${index}&avatar,person,boy,girl" alt="avatar">
-    <div class="box">
-        <h1>NAME NAME</h1>
-    </div>
-</div>
-<div class="tomato box"></div>
-*/
-
-/* 
-<li class="user-card js-user-card">
-    <div class="snip1344">
-        <img src="https://source.unsplash.com/100x100/?random=1&avatar,person,boy,girl" alt="profile-sample1" class="background"/>
-        <img src="https://source.unsplash.com/100x100/?random=1&avatar,person,boy,girl" alt="profile-sample1" class="profile"/>
-        <figcaption>
-          <h3>...<span>...</span></h3>
-            <div>adress</div>
-        </figcaption>
-    </div>
-</li>
-*/
