2d87eafb0ab5997063cd8d45f6c889c5785a6378
Пащенко Владимир <pikimelposhta@gmail.com>
2022-08-17 02:44:38 +0300

    add comments

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,9 @@
+// Створюємо глобальні змінні
 let listCoffe = [];
 let autoSaveId;
 let idShowModalInterval;
 
+// Глобальний об'єкт який зберігає посилання на елементу у HTML
 const refs = {
   formElems: document.querySelectorAll("[data-form]"),
   formBox: document.querySelector(".js-form-box"),
@@ -9,34 +11,52 @@ const refs = {
   clickBtnClose: document.querySelector(".js-close-modal"),
 };
 
+// Викликаємо функцію для ініціалізації початкових значеннь
 init();
 
-restoreElemBtn.addEventListener("click", loadDataNewElement);
+//////////////////////////////////////////////////////////////////
+//           СТВОРЮЄМО ПРОСЛУХОВУВАЧІ ПОДІЙ на елементах
+restoreElemBtn.addEventListener("click", loadDataNewElement); // клік по кнопкі видалення елемента
+btnHideModal.addEventListener("click", onHideModal); // клік по кнопці яка ховає модалку з рекламой
+autoSaveBtn.addEventListener("click", onSaveBtnClick); // клік по чекбоксу автозбереження данних
+createElemBtn.addEventListener("click", onCreateElemBtnClick); // клік по кнопці створення нового елементу
+refs.clickBtnClose.addEventListener("click", onBtnClick); // клік по кнопці закриття реклами
+refs.formBox.addEventListener("input", onInputChage); // ПОДІЯ ІНПУТ на формі (для усіх інпутів в середині цієї форми)
+//////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////
+//                     ДОДАТКОВИЙ ФУНКЦІОНАЛ
+
+//Функція збереження данних в LocalStorage( далі буду писати - ЛС)
 function saveDataToLocalStorage(key, value) {
+  //записуємо об'єкт ДЖСОН в ЛС
   localStorage.setItem(key, JSON.stringify(value));
 }
 
+//Функція завантаження данних з ЛС
 function loadDataFromLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
 
+// ФУНКЦІЯ що викликається лише один раз для ініціалізації початкових данних
 function init() {
-  listCoffe = loadDataFromLocalStorage("listCoffe");
-  showCoffe();
-  restoreData();
-  showAdvertising();
+  listCoffe = loadDataFromLocalStorage("listCoffe"); //Завантажили данні з ЛС до глобального массиву
+  showCoffe(); // Викликали функцію яка відображає массив з елементами на сторінкі
+  restoreData(); // відновлює втраченні данні - завантажуючи їх з ЛС
+  showAdvertising(); // Викликаємл функцію яка буде показувати рекламу
 }
 
+// Функція відображення массиву з елементами на сторіці
 function showCoffe() {
-  coffeElements.innerHTML = [...listCoffe]
+  let result = [...listCoffe]
     .sort((a, b) => {
+      //Стортуємо массив
       let res = 0;
       try {
         a.createdDate.getTime();
         a = new Date(a.createdDate);
         b = new Date(b.createdDate);
-        res = b.getTime() - a.getTime();
+        res = b.getTime() - a.getTime(); // Сортуємо по даті створення
       } catch (error) {
         console.log("error");
         res = -1;
@@ -45,27 +65,103 @@ function showCoffe() {
       return res;
     })
     .map(({ title, createdDate }) => {
-      return `
+      // Проходимось по відсортованному Массиву елементів
+      // Повертаємо для кожного елемента шаблон
+      return ` 
   <li class='list-item'><h5>${title}</h5><p>${createdDate}</p></li>
   `;
-    })
+    }) // Об'єднуємо массив шаблонів в один рядок (великий цілий шаблон)
     .join("");
+
+  coffeElements.innerHTML = result; // Записуємо отриманний шаблон до сторінки
 }
 
+// Функція яка зберігає усі необхідні данні у ЛС
 function saveAll() {
-  saveDataToLocalStorage("listCoffe", listCoffe);
+  saveDataToLocalStorage("listCoffe", listCoffe); // Збереження існуючого масиву
   console.log("Auto Saved!");
 }
 
-autoSaveBtn.addEventListener("click", (event) => {
+// Функція для відображення реклами
+function showAdvertising() {
+  // Створюємо інтервал на виклик реклами кожні 30хв
+  idShowModalInterval = setInterval(() => {
+    // Додаємо класс - модалка з рекламою виїзжає
+    refs.advWindow.classList.add("show");
+
+    // Цикл для зворотнього лічильника на кнопкі закриття реклами
+    for (let i = 0; i <= 5; i++) {
+      setTimeout(() => {
+        btnHideModal.value = `Close ${5 - i}`;
+      }, i * 1000);
+    }
+
+    // let i = 5;
+    // const idInter = setInterval(() => {
+    //   btnHideModal.value = `Close ${i--}`;
+    // }, 1000);
+
+    // BTN ENANBLED
+    setTimeout(() => {
+      /* ЗРОЗУМІЛА ВЕРСІЯ КОДУ НИЖЧЕ
+      if(refs.advWindow.classList.contains("show")){
+        btnHideModal.disabled = false // якщо рекламу видно то кнопка працює
+      }else{
+        btnHideModal.disabled = true // якщо рекламу не видно то кнопка не працює
+      } 
+      */
+      // clearInterval(idInter); не потрібно
+      btnHideModal.value = "Close";
+
+      // ВИШЧЕ БІЛЬ ЗРОЗУМІЛА ВЕРСІЯ ТОГО ЩО ТУТ ВІДБУВАЄТЬСЯ
+      btnHideModal.disabled = !refs.advWindow.classList.contains("show");
+    }, 5000);
+
+    //HIDE MODAL
+    //Запускаємо таймер щоб заркити рекламу через 15 секунд
+    setTimeout(() => {
+      refs.advWindow.classList.remove("show");
+    }, i * 1000 + 10000);
+  }, 60000 * 30);
+}
+
+// Функція для збереження даних створюваного елементу (який ще не встигли створити)
+function saveDateNewElement() {
+  // Отримуємо значення з інпутів на формі
+  let title = refs.formElems[0].value;
+  let desc = refs.formElems[1].value;
+  let imgUrl = refs.formElems[2].value;
+
+  // Об'єднуємо отриманні данні в об'єкт
+  let coffe = {
+    title,
+    description: desc,
+    image: imgUrl,
+  };
+
+  // Зберігаємо отриманий об'єкт в ЛС
+  saveDataToLocalStorage("newElement", coffe);
+}
+
+// Функція яка вмикає кнопку відновлення данних якщо в ЛС є об'єкт для відновлення
+function restoreData() {
+  if (localStorage.getItem("newElement")) {
+    restoreElemBtn.disabled = false;
+  }
+}
+/////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////
+//              КОЛБЕК ФУНКЦІЇ для прослуховувачів подій
+function onSaveBtnClick(event) {
   if (event.target.checked) {
     autoSaveId = setInterval(saveAll, 5000);
   } else {
     clearInterval(autoSaveId);
   }
-});
+}
 
-createElemBtn.addEventListener("click", () => {
+function onCreateElemBtnClick() {
   let title = refs.formElems[0].value;
   let desc = refs.formElems[1].value;
   let imgUrl = refs.formElems[2].value;
@@ -80,25 +176,10 @@ createElemBtn.addEventListener("click", () => {
 
   listCoffe.push(coffe);
   showCoffe();
-});
-
-refs.formBox.addEventListener("input", onInputChage);
-function onInputChage(e) {
-  saveDateNewElement();
 }
 
-function saveDateNewElement() {
-  let title = refs.formElems[0].value;
-  let desc = refs.formElems[1].value;
-  let imgUrl = refs.formElems[2].value;
-
-  let coffe = {
-    title,
-    description: desc,
-    image: imgUrl,
-  };
-
-  saveDataToLocalStorage("newElement", coffe);
+function onInputChage() {
+  saveDateNewElement();
 }
 
 function loadDataNewElement() {
@@ -110,58 +191,14 @@ function loadDataNewElement() {
   localStorage.removeItem("newElement");
 }
 
-function restoreData() {
-  if (localStorage.getItem("newElement")) {
-    restoreElemBtn.disabled = false;
-  }
-}
-
-function showAdvertising() {
-  idShowModalInterval = setInterval(() => {
-    refs.advWindow.classList.add("show");
-
-    for (let i = 0; i <= 5; i++) {
-      setTimeout(() => {
-        btnHideModal.value = `Close ${5 - i}`;
-      }, i * 1000);
-    }
-
-    // let i = 5;
-    // const idInter = setInterval(() => {
-    //   btnHideModal.value = `Close ${i--}`;
-    // }, 1000);
-
-    // BTN ENANBLED
-    setTimeout(() => {
-      /* 
-      if(refs.advWindow.classList.contains("show")){
-        btnHideModal.disabled = false
-      }else{
-        btnHideModal.disabled = true
-      } 
-      */
-      clearInterval(idInter);
-      btnHideModal.value = "Close";
-      btnHideModal.disabled = !refs.advWindow.classList.contains("show");
-    }, 5000);
-
-    //HIDE MODAL
-    setTimeout(() => {
-      refs.advWindow.classList.remove("show");
-    }, i * 1000 + 10000);
-  }, 60000 * 30);
-}
-
-refs.clickBtnClose.addEventListener("click", onBtnClick);
-
 function onBtnClick() {
   clearInterval(idShowModalInterval);
   refs.advWindow.classList.remove("show");
 }
 
-btnHideModal.addEventListener("click", onHideModal);
 function onHideModal() {
   refs.advWindow.classList.remove("show");
   clearInterval(idShowModalInterval);
   showAdvertising();
 }
+//////////////////////////////////////////////////////////////////
