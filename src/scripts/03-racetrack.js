e1c86f65f4ebcdd1f2cb14d16bbcdea9721cc6cb
Пащенко Владимир <pikimelposhta@gmail.com>
2022-08-18 17:54:00 +0300

    123

--- src/scripts/03-racetrack.js
+++ src/scripts/03-racetrack.js
@@ -1,60 +1,104 @@
 const refs = {
-  startBtn: document.querySelector(".js-start-race"),
-  winnerField: document.querySelector(".js-winner"),
-  progressField: document.querySelector(".js-progress"),
-  tableBody: document.querySelector(".js-results-table > tbody"),
+  nextBtn: document.querySelector(".js-next-page"),
+  previousBtn: document.querySelector(".js-previous-page"),
   spiner: document.querySelector(".js-spiner"),
+  listItems: document.querySelector("#listItems"),
 };
-refs.startBtn.addEventListener("click", loadDataFromServer);
+//refs.nextBtn.addEventListener("click", loadDataFromServer);
+//"https://pokeapi.co/api/v2/pokemon/"
 
-let promis = fetch("https://pokeapi.co/api/v2/pokemon/");
-let promises = [];
+let data = [];
+let globalPage = 0;
+let link = {
+  next: "https://pokeapi.co/api/v2/pokemon/",
+  previous: null,
+};
 
-let globalUrl = "https://pokeapi.co/api/v2/pokemon/";
+refs.nextBtn.addEventListener("click", onClickBtn);
+refs.previousBtn.addEventListener("click", onPreviousClickBtn);
 
-function loadDataFromServer() {
-  let promise = new Promise((resolve, reject) => {
-    console.log("Start spinner");
-    refs.spiner.classList.add("show");
-    setTimeout(() => {
-      fetch(globalUrl).then((value) => {
-        resolve(value.json());
-      });
-    }, 3000);
-  });
-
-  promise
-    .then((val) => {
-      console.log("Stop spinner");
-      globalUrl = val.next;
-      showData(val.results);
+function loadDataFromServer(linkValue) {
+  refs.spiner.classList.add("show");
+  fetch(linkValue)
+    .then((data) => {
+      return data.json();
+    })
+    .then((value) => {
+      data = value.results;
+      showItems(data);
+      link.next = value.next;
+      link.previous = value.previous;
+      saveAll();
+      refs.previousBtn.disabled = link.previous === null;
       refs.spiner.classList.remove("show");
     })
-    .catch((err) => {
-      console.log("Stop spinner");
-    });
+    .catch((error) => console.log(error));
+
+  setTimeout(() => {
+    refs.spiner.classList.remove("show");
+  }, 10000);
 }
 
-function showData(array) {
-  let result = array
-    .map((obj) => {
-      return `<li>${obj.name}</li>`;
-    })
-    .join("");
+function showItems(data) {
+  let array = data.map((e) => {
+    return `<li>${e.name}</li>`;
+  });
 
-  listItems.innerHTML = result;
+  listItems.innerHTML +=
+    `<li style='background-color:#${Math.floor(
+      Math.random() * 16777215
+    ).toString(16)}'><hr><h5>${globalPage}</h5>` +
+    `<ul>${array.join("")}</ul></li>`;
 }
 
-// function isServerOk(url) {
-//   return new Promise((resolve, reject) => {
-//     fetch(url)
-//       .then((value) => {
-//         if (value.status == 200) {
-//           resolve(true);
-//         } else {
-//           resolve(false);
-//         }
-//       })
-//       .catch((err) => console.log(err));
-//   });
-// }
+function onClickBtn(e) {
+  loadDataFromServer(link.next);
+}
+
+function onPreviousClickBtn() {
+  loadDataFromServer(link.previous);
+}
+
+function saveToLS(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+function loadFromLS(key) {
+  let item = localStorage.getItem(key);
+  try {
+    return JSON.parse(item ?? []);
+  } catch (error) {
+    return [];
+  }
+}
+
+function saveAll() {
+  saveToLS("link", link);
+  saveToLS("data", data);
+}
+
+function init() {
+  link = loadFromLS("link").map ? link : loadFromLS("link");
+  data = loadFromLS("data");
+
+  if (data.length === 0) {
+    console.log("test1");
+    loadDataFromServer(link.next);
+  } else {
+    console.log("test2");
+    showItems(data);
+  }
+}
+
+init();
+
+const BLOCK_HEIGTH = 360;
+
+listItems.addEventListener("scroll", function () {
+  let page = Math.ceil(event.target.scrollTop / BLOCK_HEIGTH);
+
+  if (page > globalPage) {
+    loadDataFromServer(link.next);
+    globalPage++;
+  }
+});
